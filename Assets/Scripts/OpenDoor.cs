87a1776ee5d90001b7be84d01e45f2662fbfdd1d
dantereto <2247@holbertonschool.com>
2022-05-08 21:02:47 -0500

    door

--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -3,31 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class OpenDoor : MonoBehaviour
+
 {
-    public Collider Rcontroller;
-    Animator anim;
-    Collider door;
-    void Awake()
+    public Animator anim;
+    public void contol()
     {
-        anim = GetComponent<Animator>();
-        door = GetComponent<BoxCollider>();
-    }
-    void FixedUpdate()
-    {
-        if (Rcontroller.bounds.Intersects(door.bounds))
-        {
-            if (anim.GetBool("open_door") == false)
-                Open();
-            else if (anim.GetBool("open_door") == true)
-                Close();
-        }
-    }
-    void Open()
-    {
-        anim.SetBool("open_door", true);
-    }
-    void Close()
-    {
-        anim.SetBool("open_door", false);
+        anim.SetBool("character_nearby", true);
     }
 }
