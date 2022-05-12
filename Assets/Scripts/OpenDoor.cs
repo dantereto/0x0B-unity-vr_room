a6f824015b555bfc04d0d1abb487d455345e6172
dantereto <2247@holbertonschool.com>
2022-05-12 16:33:39 -0500

    check

--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -6,8 +6,15 @@ public class OpenDoor : MonoBehaviour
 
 {
     public Animator anim;
+    public GameObject Canvas;
+    public GameObject Light;
     public void contol()
     {
         anim.SetBool("character_nearby", true);
+        Canvas.SetActive(false);
+    }
+    public void LighsOn()
+    {
+        Light.SetActive(true);
     }
 }
